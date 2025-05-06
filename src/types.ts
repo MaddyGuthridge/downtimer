2a4ebc46e53be3df63dc35d7018f1d357e7040b7
Maddy Guthridge <hello@maddyguthridge.com>
2025-05-06 16:28:03 +1000

    Very very basic implementation

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+
+/**
+ * A unique ID representing a timer.
+ *
+ * Under the hood, this is technically a `string`, and so can technically be JSON serialized, but
+ * saving it to a file doesn't make very much sense, since the timers will all be killed when the
+ * process exits.
+ */
+export type TimerId = string & { __timerId: '__timerId' };
+
+/**
+ * A callback function, executed once a timer fires.
+ */
+export type TimerCallback = () => void;
