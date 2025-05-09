2a9659735a14c322c773bd9c012c08c18ad742d3
Maddy Guthridge <hello@maddyguthridge.com>
2025-05-09 17:42:50 +1000

    More tests for scheduler

--- tests/scheduler.test.ts
+++ tests/scheduler.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
 import { downtimer } from '../src';
 import { sleep } from './util';
 
@@ -11,7 +11,15 @@ describe('Scheduler', () => {
     expect(callback).toBeCalledTimes(1);
   });
 
-  it.only('Allows timers to be cleared', async () => {
+  test("Callbacks aren't called until their scheduled time", async () => {
+    const dt = downtimer();
+    const callback = vi.fn();
+    dt.schedule(callback, 10);
+    await sleep(5);
+    expect(callback).not.toBeCalled();
+  });
+
+  it('Allows timers to be cleared', async () => {
     const dt = downtimer();
     const callback = vi.fn();
     const timer = dt.schedule(callback, 10);
@@ -24,5 +32,40 @@ describe('Scheduler', () => {
     await sleep(10);
 
     expect(callback).not.toBeCalled();
+  });
+
+  it('Allows all timers to be cleared', async () => {
+    const dt = downtimer();
+    const callbacks = [vi.fn(), vi.fn(), vi.fn()];
+
+    callbacks.forEach(cb => dt.schedule(cb, 5));
+
+    dt.clearAll();
+
+    await sleep(10);
+
+    callbacks.forEach(cb => {
+      expect(cb).not.toBeCalled();
+    });
+  });
+
+  test('Clearing an already-fired timer is a no-op', async () => {
+    const dt = downtimer();
+    const timer = dt.schedule(() => {}, 5);
+    await sleep(10);
+    dt.clear(timer);
+  });
+
+  test("Clearing a timer doesn't clear other timers", async () => {
+    const dt = downtimer();
+    // First timer gets cancelled
+    const timer1 = dt.schedule(() => {}, 5);
+    // Second timer not cancelled
+    const callback = vi.fn();
+    dt.schedule(callback, 5);
+    
+    dt.clear(timer1);
+    await sleep(10);
+    expect(callback).toBeCalled();
   });
 });
