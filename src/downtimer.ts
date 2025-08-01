10355a39fbfd3af268f5c1b1099ef1d1582a155d
Maddy Guthridge <hello@maddyguthridge.com>
2025-08-01 16:29:00 +1000

    Update inline documentation to document common pitfalls

--- src/downtimer.ts
+++ src/downtimer.ts
@@ -30,7 +30,16 @@ export class Downtimer {
   /** Color options */
   #colors: Colors;
 
-  /** Create a new instance of the Downtimer manager. */
+  /**
+   * Create a new Downtimer manager object.
+   *
+   * Importantly, this manager can only access timers that it scheduled. As such, creating a new
+   * `downtimer` object elsewhere in your code won't allow you to clear timers scheduled using a
+   * different timer manager. As such, it may be best to create a single global `downtimer` manager
+   * which you use across your entire codebase.
+   *
+   * @param options Options for the `Downtimer` object, including settings for logging.
+   */
   constructor(options: DeepPartial<DowntimerOptions> = {}) {
     this.#options = mergeDeepPartial(defaultOptions, options);
     this.#timers = {};
@@ -71,7 +80,10 @@ export class Downtimer {
    *
    * @param callback callback function to schedule
    * @param ms amount of time to wait before calling the callback.
-   * @returns a timer ID which can be used to cancel the timer.
+   *
+   * @returns a timer ID which can be used to cancel the scheduled timer. You should store this
+   * timer ID somewhere in your data structures if there is a chance that you'll need to cancel it
+   * later.
    */
   schedule(callback: TimerCallback, ms: number): TimerId {
     const externalId = uuid() as TimerId;
@@ -96,10 +108,15 @@ export class Downtimer {
 
   /**
    * Cancel the scheduled callback function associated with the given timer ID. If the callback
-   * hasn't been run yet, it will be cancelled and will never run. If the callback has already been
-   * run, a warning will be logged.
+   * hasn't been run yet, it will be cancelled and will never run.
    *
-   * @param timerId ID of timer to cancel
+   * If the scheduled callback has already been run, or does not exist, a warning will be logged to
+   * the console. Importantly, no exception will be thrown.
+   *
+   * Note that the scheduled timer must be cancelled using the `downtimer` manager that scheduled
+   * it. Other `downtimer` manager instances will log a "timer not found" warning.
+   *
+   * @param timerId ID of timer to clear
    */
   clear(timerId: TimerId) {
     const timer = this.#timers[timerId];
@@ -116,6 +133,9 @@ export class Downtimer {
 
   /**
    * Cancel all outstanding callback functions.
+   *
+   * This cancels all scheduled timers managed by this `downtimer` object. Note that timers
+   * scheduled by other `downtimer` manager objects won't be cleared.
    */
   clearAll() {
     logClearAll(this.#options.logConfig.clear.allOutstanding, this.#timers, this.#colors);
@@ -139,7 +159,12 @@ export class Downtimer {
 }
 
 /**
- * Create a new instance of the Downtimer manager.
+ * Create a new Downtimer manager object.
+ *
+ * Importantly, this manager can only access timers that it scheduled. As such, creating a new
+ * `downtimer` object elsewhere in your code won't allow you to clear timers scheduled using a
+ * different timer manager. As such, it may be best to create a single global `downtimer` manager
+ * which you use across your entire codebase.
  *
  * @param options Options for the `Downtimer` object, including settings for logging.
  */
