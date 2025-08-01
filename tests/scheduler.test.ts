6b694db3ce0942d1ce0f74bd82e3a09e98d1868b
Maddy Guthridge <hello@maddyguthridge.com>
2025-08-01 15:45:21 +1000

    update scheduler tests to use constants for wait durations

--- tests/scheduler.test.ts
+++ tests/scheduler.test.ts
@@ -2,34 +2,49 @@ import { describe, expect, it, test, vi } from 'vitest';
 import { downtimer } from '../src';
 import { optionsNoLogging, sleep } from './util';
 
+/** Maximum acceptable variance in timers is 10ms */
+const MIN_VARIANCE = 10;
+
+/** Short timer is 5ms */
+const SHORT = 5;
+/** After this time, short timer definitely should have fired */
+const AFTER_SHORT = SHORT + MIN_VARIANCE;
+
+/** Long timer is 15ms */
+const LONG = 15;
+/** Before this time, long timer definitely shouldn't have fired */
+const BEFORE_LONG = LONG - MIN_VARIANCE;
+/** Before this time, long timer definitely should have fired */
+const AFTER_LONG = LONG + MIN_VARIANCE;
+
 describe('Scheduler', () => {
   it.concurrent('Schedules timers', async () => {
     const dt = downtimer(optionsNoLogging);
     const callback = vi.fn();
-    dt.schedule(callback, 10);
-    await sleep(15);
+    dt.schedule(callback, LONG);
+    await sleep(AFTER_LONG);
     expect(callback).toBeCalledTimes(1);
   });
 
   test.concurrent("Callbacks aren't called until their scheduled time", async () => {
     const dt = downtimer(optionsNoLogging);
     const callback = vi.fn();
-    dt.schedule(callback, 10);
-    await sleep(5);
+    dt.schedule(callback, LONG);
+    await sleep(BEFORE_LONG);
     expect(callback).not.toBeCalled();
   });
 
   it.concurrent('Allows timers to be cleared', async () => {
     const dt = downtimer(optionsNoLogging);
     const callback = vi.fn();
-    const timer = dt.schedule(callback, 10);
-    await sleep(5);
+    const timer = dt.schedule(callback, LONG);
+    await sleep(BEFORE_LONG);
 
     // Cancel timer
     dt.clear(timer);
 
     // Wait for when the timer would have been fired
-    await sleep(10);
+    await sleep(AFTER_LONG - BEFORE_LONG);
 
     expect(callback).not.toBeCalled();
   });
@@ -38,11 +53,11 @@ describe('Scheduler', () => {
     const dt = downtimer(optionsNoLogging);
     const callbacks = [vi.fn(), vi.fn(), vi.fn()];
 
-    callbacks.forEach(cb => dt.schedule(cb, 5));
+    callbacks.forEach(cb => dt.schedule(cb, SHORT));
 
     dt.clearAll();
 
-    await sleep(10);
+    await sleep(AFTER_SHORT);
 
     callbacks.forEach(cb => {
       expect(cb).not.toBeCalled();
@@ -51,21 +66,22 @@ describe('Scheduler', () => {
 
   test.concurrent('Clearing an already-fired timer is a no-op', async () => {
     const dt = downtimer(optionsNoLogging);
-    const timer = dt.schedule(() => {}, 5);
-    await sleep(10);
+    const timer = dt.schedule(() => {}, SHORT);
+    await sleep(AFTER_SHORT);
     dt.clear(timer);
   });
 
   test.concurrent("Clearing a timer doesn't clear other timers", async () => {
     const dt = downtimer(optionsNoLogging);
     // First timer gets cancelled
-    const timer1 = dt.schedule(() => {}, 5);
+    const timer1 = dt.schedule(() => {}, LONG);
     // Second timer not cancelled
     const callback = vi.fn();
-    dt.schedule(callback, 5);
+    dt.schedule(callback, LONG);
 
     dt.clear(timer1);
-    await sleep(10);
+
+    await sleep(AFTER_LONG);
     expect(callback).toBeCalled();
   });
 });
